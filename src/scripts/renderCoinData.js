75240e682ed75696d2982feba99160fc207357df
Kevin Smith <kevinl2code@gmail.com>
2020-10-23 14:21:54 -0500

    added keys to map function

--- src/scripts/renderCoinData.js
+++ src/scripts/renderCoinData.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default (currency) => {
     const data = Object.entries(currency)
-    return data.map((entry) => {
+    return data.map((entry, index) => {
         return(
-            <tr>
+            <tr key={index}>
                 <td data-label="Key">{entry[0]}</td>
                 <td data-label="Value">{entry[1]}</td>
             </tr>
